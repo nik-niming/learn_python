b28151d678ee5e0a8a280035c149aeba2f1462a4
nik.niming <nik.niming@gmail.com>
2016-08-27 18:53:01 +0800

    基于httplib的web服务器检查

--- network/web_server_checker_httplib.py
+++ network/web_server_checker_httplib.py
@@ -3,10 +3,12 @@
 
 import httplib
 import sys
+import socket
+
 
 def check_webserver(address, port, resource):
     # create connection
-    if not resource.startwith('/'):
+    if not resource.startswith('/'):
         resource = '/' + resource
 
     try:
@@ -18,5 +20,33 @@ def check_webserver(address, port, resource):
         # get ressponse
         resp = conn.getresponse()
         print "response status:%s" % resp.status
-    except:
-        psass
+    except socket.error, e:
+        print "HTTP connetion failed:%s" % e
+        return False
+    finally:
+        conn.close()
+        print "HTTP connection close successfully"
+
+    if resp.status in [200, 301]:
+        return True
+    else:
+        return False
+
+if __name__ == '__main__':
+    from optparse import OptionParser
+
+    parser = OptionParser()
+
+    parser.add_option("-a", "--address", dest="address", default="localhost",
+                      help="Address for web server", metavar="ADDRESS")
+    parser.add_option("-p", "--port", dest="port", type="int", default="80",
+                      help="Port for web server", metavar="PORT")
+    parser.add_option("-r", "--resource", dest="resource", default="index.html",
+                      help="Resource to check", metavar="RESOURCE")
+
+    (options, args) = parser.parse_args()
+    print "options: %s, args: %s" % (options, args)
+
+    check = check_webserver(options.address, options.port, options.resource)
+    print "check_webserver returned %s" % check
+    sys.exit(not check)
