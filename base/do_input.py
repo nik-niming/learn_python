684184715d1ed8e67f521d5998af2f7d24b02267
nik_niming <10546248@qq.com>
2019-04-02 16:50:57 +0800

    第一个python程序,输入和输出

---
+++ base/do_input.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+__author__ = 'liaoxuefeng.com'
+
+# print()函数也可以接受多个字符串，用逗号“,”隔开，就可以连成一串输出：
+print('The quick brown fox', 'jump over', 'the lazy dog')
+
+# 可以把计算100 + 200的结果打印得更漂亮一点：
+print('100 + 200 =', 100 + 200)
+
+# Python提供了一个input()，可以让用户输入字符串，并存放到一个变量里。比如输入用户的名字：
+name = input('please enter your name:')
+print('hello,', name)
+
+# 练习: 请利用print()输出1024 * 768 = xxx：
